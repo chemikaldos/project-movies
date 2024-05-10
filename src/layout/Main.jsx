165e76b5fe617dc9a51fae1987e542c614f52aa9
Dmitry Mukhriakov <dbike@mail.ru>
2024-05-11 00:40:11 +0400

    дораболтал

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -4,8 +4,10 @@ import { Search } from "../components/Search";
 import Preloader from "../components/Preloader";
 
    const API_KEY =process.env.REACT_APP_API_KEY;
+   console.log(API_KEY);
 
    class Main extends React.Component{
+    
     state={
         movies:[],
         loading: true
@@ -14,15 +16,23 @@ import Preloader from "../components/Preloader";
  
 
 componentDidMount(){
-    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
+    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
     .then(response =>response.json())
     .then(data=>this.setState({movies: data.Search, loading: false}))
+    .catch((err)=>{
+        console.error(err);
+        this.setState({loading: false})
+    })
 }
 searchMovies = (str, type='all')=>{
     this.setState({loading: true})
-    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
+    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
     .then(response =>response.json())
     .then(data=>this.setState({movies: data.Search, loading: false}))
+    .catch((err)=>{
+        console.error(err);
+        this.setState({loading: false})
+    })
 }
 
     render(){
